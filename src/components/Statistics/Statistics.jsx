bdc72047cd73413dd1e5fbd023ff403ac15d7c27
Evgeniy <yaremenko.x@gmail.com>
2023-05-23 12:18:28 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
-import { getRandomHexColor } from './getRandomHexColor';
+import { getRandomHexColor } from './getRandomHexColor.js';
 
 export const Statistics = ({ title, stats }) => {
   return (
